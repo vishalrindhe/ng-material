5c61579b41ae7446a233c44f895ccff292f0bc00
vishalrindhe <rindhevishal32@gmail.com>
2020-09-10 02:05:41 +0530

    states added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Reg } from './reg';
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
@@ -9,11 +10,31 @@ import { FormGroup } from '@angular/forms';
 })
 export class AppComponent {
   title = 'angular-material';
+  stateHasError = true;
+
   
+  states = [ "Andhra Pradesh","Arunachal Pradesh",
+            "Assam","Bihar","Chhattisgarh", "Goa", "Gujarat","Haryana",
+            "Himachal Pradesh","Jharkhand","Karnataka","Kerala",
+            "Madhya Pradesh","Maharashtra","Manipur"," Meghalaya",
+            " Mizoram"," Nagaland","Odisha","Punjab","Rajasthan",
+            "Sikkim","Tamil Nadu","Telangana","Tripura",
+            "Uttar Pradesh","Uttarakhand","West Bengal"];
+
+  userModel = new Reg ( " ", " ", " " , 1 , 2 , "State "," "," " ,true);
+
   emailFormControl = new FormControl('', [
     Validators.required,
     Validators.email,
   ]);
+
+  validateTopic(value) {
+    if (value === 'default'){
+    this.stateHasError=true;
+    } else {
+      this.stateHasError = false;
+    }
+  }
 
   onChange(event) {
     console.log(event);
---
+++ src/app/reg.ts
@@ -0,0 +1,13 @@
+export class Reg {
+    constructor(
+        public firstName: string,
+        public lastName:string,
+        public email: string,
+        public phone: number,
+        public age: number,
+        public state:string,
+        public country:string,
+        public address:string,
+        public subscribe: boolean,
+    ) {}
+}
